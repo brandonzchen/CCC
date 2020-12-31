93a8e3ea0b297d44e7b6ba1c69a22d2ccd8c75e8
Brandon Chen <70297470+brandonzchen@users.noreply.github.com>
2020-12-30 19:41:48 -0700

    Add files via upload

--- j5.py
+++ j5.py
@@ -1,12 +1,18 @@
+import sys
+
+minimum = int(sys.stdin.readline())
+maximum = int(sys.stdin.readline())
+numnewlocations = int(sys.stdin.readline())
+
 unsortedmotels = [0, 990, 1010, 1970, 2030, 2940, 3060, 3930, 4060, 4970, 5030, 5990, 6010, 7000]
-newlocations = []
-for i in newlocations:
-    unsortedmotels.append(i)
-sortedmotels = sorted(unsortedmotels)
 ways = [1]
-minimum = 970
-maximum = 1030
 count = 0
+
+for i in range(numnewlocations):
+    tempnewlocation = int(sys.stdin.readline())
+    unsortedmotels.append(tempnewlocation)
+sortedmotels = sorted(unsortedmotels)
+
 for i in sortedmotels:
     if i  - minimum < 0:
         pass
@@ -19,5 +25,6 @@ for i in sortedmotels:
                     count += ways[sortedmotels.index(j)]
         ways.append(count)
         count = 0
+
 final = len(ways) - 1
 print(ways[final])
