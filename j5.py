50952fc2b109f14b5f405b22fefa5757560598ba
Brandon Chen <70297470+brandonzchen@users.noreply.github.com>
2020-12-30 19:34:20 -0700

    Add files via upload

--- j5.py
+++ j5.py
@@ -1,7 +1,7 @@
 unsortedmotels = [0, 990, 1010, 1970, 2030, 2940, 3060, 3930, 4060, 4970, 5030, 5990, 6010, 7000]
 newlocations = []
 for i in newlocations:
-    motels.append(i)
+    unsortedmotels.append(i)
 sortedmotels = sorted(unsortedmotels)
 ways = [1]
 minimum = 970
